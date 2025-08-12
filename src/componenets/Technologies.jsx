61313fdae15415fb2a6c21342ac3886a6169982b
abdelac <77166843+abdelac@users.noreply.github.com>
2025-08-13 00:34:15 +0100

    Update Technologies.jsx

--- src/componenets/Technologies.jsx
+++ src/componenets/Technologies.jsx
@@ -90,43 +90,4 @@ const Technologies = () => {
 
 
 
-
-
-
-const Certificates = () => {
-  return (
-    <div className='border-b border-neutral-800 pb-24'>
-        <motion.h2
-        whileInView={{opacity:1,y:0}}
-        initial={{opacity:0,y:-80}}
-        transition={{duration:1.5}}
-         className='my-20 text-center text-4xl'>Certificates</motion.h2>
-        <motion.div 
-        whileInView={{opacity:1,x:0}}
-        initial={{opacity:0,x:-100}}
-        transition={{duration:1}}
-        className="flex flex-wrap items-center justify-center gap-4">
-
-            <motion.div 
-            variants={iconvariants(3)}
-            initial="inital"
-            animate="animate"className="rounded-2xl border-4 border-neutral-800 p-4">
-                <SiAngular   className='text-7xl text-red-600'/>
-            </motion.div>
-
-           
-        
-
-     
-           
-          
-        </motion.div>
-
-
-    
-    </div>
-  )
-}
-
-export default Certificates
 export default Technologies
